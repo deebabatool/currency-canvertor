70d09719cc7ea09c063580990065ad4ec0527ae3
deebabatool <161479432+deebabatool@users.noreply.github.com>
2024-05-30 19:25:05 +0500

    currency contertor

---
+++ index.js
@@ -0,0 +1,28 @@
+import inquirer from "inquirer";
+const currency = {
+    USD: 1,
+    EUR: 0.93,
+    IND: 74.57,
+    PKR: 200,
+};
+let user_answer = await inquirer.prompt([
+    {
+        name: "from",
+        message: "Enter from currency",
+        type: "list",
+        choices: ['USD', 'EUR', 'IND', 'PKR',]
+    },
+    {
+        name: "to",
+        message: "Enter from currency",
+        type: "list",
+        choices: ['USD', 'EUR', 'IND', 'PKR',]
+    },
+    {
+        name: 'amount',
+        message: "Enter your Amount",
+        type: 'number',
+    },
+]);
+console.log(user_answer);
+//let fromamount = 
